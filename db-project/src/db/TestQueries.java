20c8897f13e6061a109077a4ac4b6212fe56045f
Brian J. Walters <brian.joseph.walters@gmail.com>
2013-11-09 12:33:26 -0600

    added tests

--- db-project/src/db/TestQueries.java
+++ db-project/src/db/TestQueries.java
@@ -1,10 +1,15 @@
 package db;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-public class TestQueries {
+import java.sql.Connection;
+import java.sql.SQLException;
 
-	public ResultSet getCompanyEmployees(String company_id) {
-		PreparedStatement stmt = conn.preparedStatement(
+public class TestQueries {
+	private Connection conn;
+	
+	public ResultSet getCompanyEmployees(String company_id) 
+			throws SQLException {
+		PreparedStatement stmt = conn.prepareStatement(
 			"SELECT last_name, first_name " +
 			"FROM person NATURAL JOIN employment NATURAL JOIN " +
 			"	  job NATURAL JOIN company " +
@@ -14,8 +19,9 @@ public class TestQueries {
 		return rset;
 	}
 
-	public ResultSet getCompanyFullTimeEmployeesBySalaryDesc(String company_code) {
-		PreparedStatement stmt = conn.preparedStatement(
+	public ResultSet getCompanyFullTimeEmployeesBySalaryDesc(String company_code) 
+			throws SQLException {
+		PreparedStatement stmt = conn.prepareStatement(
 			"SELECT last_name, first_name, pay_rate" +
 			"FROM person NATURAL JOIN employment NATURAL JOIN " +
 			"	  job NATURAL JOIN company " +
@@ -26,8 +32,9 @@ public class TestQueries {
 		return rset;
 	}
 
-	public ResultSet getCompanyTotalLaborCost(String company_code) {
-		PreparedStatement stmt = conn.preparedStatement(
+	public ResultSet getCompanyTotalLaborCost(String company_code) 
+			throws SQLException {
+		PreparedStatement stmt = conn.prepareStatement(
 			"SELECT sum(calculated_wage)" +
 			"FROM person NATURAL JOIN employment NATURAL JOIN " +
 			"	  job NATURAL JOIN company " +
